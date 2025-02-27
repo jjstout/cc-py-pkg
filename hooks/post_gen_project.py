c16ef090d68a7770c9578bcea197f0f52bfe3c34
Justin Stout <justin@jstout.us>
2025-02-27 15:56:41 +0000

    add disable cli

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,3 +6,6 @@ ctx_out.parent.mkdir(parents=True, exist_ok=True)
 
 with ctx_out.open("w") as fd_out:
     fd_out.write("""{{ cookiecutter | jsonify }}\n""")
+
+if '{{ cookiecutter.enable_cli}}' == "False":
+    Path("src/{{ cookiecutter.pkg_slug }}/cli.py").unlink()
