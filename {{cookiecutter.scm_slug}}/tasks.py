934ad2a0c121cf1b717f8ebfa555d45382f5c180
Justin Stout <justin@jstout.us>
2025-02-27 16:42:03 +0000

    minor fixes

--- {{cookiecutter.scm_slug}}/tasks.py
+++ {{cookiecutter.scm_slug}}/tasks.py
@@ -14,20 +14,96 @@ with Path("project.d/cc-ctx.json").open() as fd_in:
     }
 
     CFG = CFG | CC_CTX
-    CFG["pkg_idx_url"] = CFG["PYPI_TEST_URL"]
-    CFG["pkg_idx_user"] = CFG["PYPI_TEST_USER"]
-    CFG["pkg_idx_passwd"] = CFG["PYPI_TEST_PASSWD"]
-    
-    if CFG.get("enable_gitflow", False):
-        REMOTE_BRANCHES.extend(["develop"])
+    CFG["pkg_idx_url"] = CFG["PYPI_URL"]
+    CFG["pkg_idx_user"] = CFG["PYPI_USER"]
+    CFG["pkg_idx_passwd"] = CFG["PYPI_PASSWD"]
 
 
 @task
-def pp_cfg(ctx):
-    """Pretty print the project configuration."""
-    from pprint import pprint as pp
-    pp(CFG)
-    pp(REMOTE_BRANCHES)
+def bumpversion(ctx, part):
+    """Bump the version number, by major, minor or patch/hotfix"""
+    part = "patch" if part == "hotfix" else part
+
+    ctx.run(f"uv run bump-my-version bump {part}")
+    ctx.run(f"uv lock --upgrade-package {CFG['scm_slug']}")
 
 
-ns = Collection(pp_cfg)
+@task
+def clean(ctx):
+    """Delete build, dist, cache and other generated files."""
+    patterns = [
+        "build/",
+        "dist/",
+        ".mypy_cache/",
+        ".pytest_cache/",
+        ".ruff_cache/",
+    ]
+
+    for pattern in patterns:
+        ctx.run(f"rm -rf {pattern}")
+
+    ctx.run("find . -name '__pycache__' -exec rm -rf {} +")
+
+
+@task
+def init(ctx):
+    """Initialize git repository. Run this task after cloning the repository."""
+    if not Path(".gitignore").exists():
+        raise Failure(".gitignore file not found")
+
+    if not Path(".git").exists():
+        ctx.run("git init")
+        ctx.run(f"git remote add origin {CFG['scm_repo_url']}")
+        ctx.run("git add .")
+        ctx.run('git commit -m "Initial commit from jjstout/cc-py-pkg template"')
+
+        if CFG.get("enable_gitflow", False):
+            REMOTE_BRANCHES.extend(["develop"])
+            ctx.run("git flow init -d")
+    
+        for branch in REMOTE_BRANCHES:
+            ctx.run(f"git push -u origin {branch}")
+
+
+@task
+def lint(ctx):
+    """Format and lint the project code."""
+    ctx.run("uv run ruff format src/")
+    ctx.run("uv run ruff check --fix src/")
+    ctx.run("uv run pydocstyle src/")
+
+
+@task
+def push(ctx):
+    """Push all remote branches and tags to the remote repository."""
+    for branch in REMOTE_BRANCHES:
+        ctx.run(f"git push origin {branch}")
+
+    ctx.run("git push --tags")
+
+
+@task
+def test(ctx):
+    """Run the project tests."""
+    ctx.run("uv run pytest src/")
+    ctx.run("uv run mypy src/")
+    
+
+@task(clean)
+def build(ctx):
+    """Build the project."""
+    ctx.run("uv build")
+
+
+@task(build)
+def release(ctx):
+    """Release the project."""
+    ctx.run(
+        f"""uv run twine upload --repository-url {CFG['pkg_idx_url']} \
+                                --username {CFG['pkg_idx_user']} \
+                                --password {CFG['pkg_idx_passwd']} \
+                                dist/*"""
+    )
+
+
+ns = Collection(build, bumpversion, clean, init, lint, push, release, test)
