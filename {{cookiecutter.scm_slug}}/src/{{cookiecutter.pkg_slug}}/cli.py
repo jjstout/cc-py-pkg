b6329cc64cc9b153e2f6932a4ff22facdc1bcba3
Justin Stout <justin@jstout.us>
2025-06-25 19:58:44 +0000

    add --version to cli

--- {{cookiecutter.scm_slug}}/src/{{cookiecutter.pkg_slug}}/cli.py
+++ {{cookiecutter.scm_slug}}/src/{{cookiecutter.pkg_slug}}/cli.py
@@ -9,15 +9,27 @@ import sys
 
 import typer
 from loguru import logger
+from typing import Optional
 from typing_extensions import Annotated 
 
 LOG_LEVELS = ["WARNING", "INFO", "DEBUG", "TRACE"]
 
 app = typer.Typer()
 
+def version_callback(value: bool):
+    from . import __VERSION__
+    if value:
+        typer.echo(f"Version: {__VERSION__}")
+        raise typer.Exit()
+
+
 @app.command()
 def main(
     verbose: Annotated[int, typer.Option("--verbose", "-v", count=True, max=3, clamp=True)] = 0,
+    version: Annotated[
+        Optional[bool],
+        typer.Option("--version", callback=version_callback, is_eager=True),
+    ] = None,
     quite: Annotated[bool, typer.Option("--quite", "-q", help="Suppress log messages")] = False,
     ) -> None:
   
